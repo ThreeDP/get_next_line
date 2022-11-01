bfb11e0ade1823ca161783fd08eaedb7866a63a8
Threedp <davy.paulino@aluno.ifsp.edu.br>
2022-11-01 19:19:22 -0300

    t6

--- srcs/get_next_line.c
+++ srcs/get_next_line.c
@@ -60,7 +60,7 @@ char	*ft_strdup(const char *s, size_t size)
 {
 	char	*ptr;
 
-	ptr = (char *) malloc(size * sizeof(char) + 1);
+	ptr = (char *) calloc(size + 1, sizeof(char));
 	if (!ptr)
 		return (NULL);
 	ft_strlcpy(ptr, s, size + 1);
