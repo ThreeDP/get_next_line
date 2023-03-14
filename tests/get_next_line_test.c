3a0b1ebb08a41ae551915dfd2660f8e3d921401e
threedp <davy.paulino@aluno.ifsp.edu.br>
2023-03-14 03:55:56 -0300

    TESTS: Att workflows tests and add a new test for mandatory part

--- tests/get_next_line_test.c
+++ tests/get_next_line_test.c
@@ -1,6 +1,23 @@
 #include "minunit-42/minunit.h"
 #include "../srcs/get_next_line.h"
 
+MU_TEST_SUITE(test_pass_a_file_with_one_line_with_42_char_and_no_new_line)
+{
+	//ARRANGE
+    int			fd						= open("./files/42_no_nl", O_RDONLY);
+    char        *str1;
+    char        *expected_str1          = "012345678901234567890123456789012345678901";
+
+    //ACT
+    str1 = get_next_line(fd);
+
+    //ASSERT
+    mu_assert_int_eq(g_ft_strlen(expected_str1), g_ft_strlen(str1));
+    mu_assert_string_eq(expected_str1, str1);
+    if (str1)
+        free(str1);
+}
+
 MU_TEST_SUITE(test_pass_a_file_with_one_line_with_42_char_more_a_new_line_and_other_line_with_one_char)
 {
 	//ARRANGE
@@ -27,6 +44,7 @@ MU_TEST_SUITE(test_pass_a_file_with_one_line_with_42_char_more_a_new_line_and_ot
 
 MU_TEST_SUITE(test_suite)
 {	
+    MU_RUN_TEST(test_pass_a_file_with_one_line_with_42_char_and_no_new_line);
     MU_RUN_TEST(test_pass_a_file_with_one_line_with_42_char_more_a_new_line_and_other_line_with_one_char);
 }
 
